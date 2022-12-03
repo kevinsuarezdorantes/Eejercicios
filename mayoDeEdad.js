6892d3e1cc9ab6d1e795b7bc352975ab1592a225
unknown <kevinsuarezdorantes@gmail.com>
2022-12-02 22:03:33 -0600

    creacion del ejercicios de las figuras

--- mayoDeEdad.js
+++ mayoDeEdad.js
@@ -22,4 +22,50 @@ async function verificarEdad() {
 
 
 
+const btnFigura = document.getElementById('btnFig');
+
+
+
+btnFigura.onclick = () => {
+    calcularArea();
+}
+
+
+async function calcularArea() {
+
+    var figura = prompt("¿De que figura quieres calcular el area?, ingresa: a-->cuadrado  b-->triangulo")
+
+    var altura = 0;
+var base = 0;
+
+var lado = 0;
+
+if (figura == "b") {
+
+
+altura = prompt("ingresa la medida  altura")
+base = prompt("ingresa la medida  base")
+var calculart = (base * altura)/2;
+alert("el area de tu figura es: " + calculart)
+
+
+} else {
+
+    if (figura == "a") {
+
+
+        lado = prompt("ingresa la medida de uno de los lados del cuadrado")
+        var calcularc = lado * lado;
+        alert("el area de tu figura es: " + calcularc)
+        
+        
+        } else {
+        
+            alert("Ey you vos sos pero retrasado o te pario un teletubie? porque pusiste una opcion que no esta ehh, considera ir al psicologo")
+            
+        }
+    
+}
+
+}
 
