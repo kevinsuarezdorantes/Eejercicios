de63f8535ba70c1ad5d75fa22dbeff77dc9bd406
unknown <kevinsuarezdorantes@gmail.com>
2022-12-02 21:28:33 -0600

    creacion del ejercicio de edades

--- mayoDeEdad.js
+++ mayoDeEdad.js
@@ -1,9 +1,25 @@
-var edad = 18
-var edad_usuario = prompt("ingresa tu edad")
+const btnIniciar = document.getElementById('btn');
 
-if(edad_usuario >= edad){
-    alert("ay perro ya puedes ir a la carcel legalmente")
-}else{
 
-    alert("Hijole carnal no puedes votar por un presidente aun, te me esperas :(")
+btnIniciar.onclick = () => {
+    verificarEdad();
 }
+
+
+async function verificarEdad() {
+    var edad = 18
+    var edad_usuario = prompt("ingresa tu edad")
+
+    if (edad_usuario >= edad) {
+        alert("ay perro ya puedes ir a la carcel legalmente")
+    } else {
+
+        alert("Hijole carnal no puedes votar por un presidente aun, te me esperas :(")
+    }
+
+}
+
+
+
+
+
