1cfe27d5cf7731e5fa7716bc530becee770d19ca
unknown <kevinsuarezdorantes@gmail.com>
2022-12-13 23:48:34 -0600

    creacion del ejercicio de comparacion de numeros

--- mayoDeEdad.js
+++ mayoDeEdad.js
@@ -1,3 +1,60 @@
+
+const btnNum = document.getElementById('btnNumMayor');
+
+
+btnNum.onclick = () => {
+    compararNumero();
+}
+
+
+
+
+async function compararNumero() {
+
+    var num1 = prompt("Ingrese su primer numero por favor");
+    var num2 = prompt("Ingrese su segundo numero por favor");
+    var num3 = prompt("Ingrese tercer numero por favor");
+
+    if (num1 > num2 && num1 > num3) {
+
+        alert("El número " + num1 + " es el mayor de todos");
+
+    } 
+    if (num2 > num1 && num2 > num3) {
+
+        alert("El número " + num2 + " es el mayor de todos");
+
+    }
+    
+    if (num3 > num1 && num3 > num2) {
+
+        alert("El número " + num3 + " es el mayor de todos");
+
+    }
+
+}
+
+
+const btnInventario = document.getElementById('btnInventario');
+
+var inventario = {
+    "sopa": 12,
+    "cacahuates": 10,
+    "nork suiza": 54
+};
+
+async function consultarStock() {
+    var producto = prompt("que es lo que quires buscar?");
+
+    if (inventario.hasOwnProperty(producto)) {
+
+        alert("Del producto: " + producto + " hay en stock: " + inventario[producto] + " unidades");
+    } else {
+        alert("No se encontro el producto " + producto);
+    }
+}
+
+
 const btnIniciar = document.getElementById('btn');
 
 
@@ -36,36 +93,36 @@ async function calcularArea() {
     var figura = prompt("¿De que figura quieres calcular el area?, ingresa: a-->cuadrado  b-->triangulo")
 
     var altura = 0;
-var base = 0;
+    var base = 0;
 
-var lado = 0;
+    var lado = 0;
 
-if (figura == "b") {
-
-
-altura = prompt("ingresa la medida  altura")
-base = prompt("ingresa la medida  base")
-var calculart = (base * altura)/2;
-alert("el area de tu figura es: " + calculart)
+    if (figura == "b") {
 
 
-} else {
+        altura = prompt("ingresa la medida  altura")
+        base = prompt("ingresa la medida  base")
+        var calculart = (base * altura) / 2;
+        alert("el area de tu figura es: " + calculart)
 
-    if (figura == "a") {
+
+    } else {
+
+        if (figura == "a") {
 
 
-        lado = prompt("ingresa la medida de uno de los lados del cuadrado")
-        var calcularc = lado * lado;
-        alert("el area de tu figura es: " + calcularc)
-        
-        
+            lado = prompt("ingresa la medida de uno de los lados del cuadrado")
+            var calcularc = lado * lado;
+            alert("el area de tu figura es: " + calcularc)
+
+
         } else {
-        
+
             alert("Ey you vos sos pero retrasado o te pario un teletubie? porque pusiste una opcion que no esta ehh, considera ir al psicologo")
-            
+
         }
-    
-}
+
+    }
 
 }
 
